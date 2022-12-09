3bdb01490514622d74ae36ffcc4befaa6bf97ba7
Mike McDougal <mike@mcdoogs.io>
2022-12-09 15:22:59 -0500

    add basic config, navigation, npm update

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        },
+    };
+};
